cd04be86cf9085b7d186326c4b0c53cea77c00b6
Helen Sayer <helen.sayer10@gmail.com>
2020-09-11 19:51:12 +0000

    Adds index.html to load Jasmine, sets initial spec and script, dir and files.

---
+++ spec/calcDrink.js
@@ -0,0 +1,10 @@
+describe("whatCanIDrink", function()  {
+    describe("ChecksAge function", function()   {
+        it("should return true", function() {
+            expect(checksAge < 14).toEqual(true);
+        });
+    });
+});
+
+
+
